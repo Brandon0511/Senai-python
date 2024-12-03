9ec8c663c5ad9994b115e6c29e62f4501e64db25
Brandon0511 <brandonericktenagutierrez@gmail.com>
2024-12-03 09:50:54 -0300

    Python -Aula 2
    Programação máxima.

---
+++ Pyhon - Aula 2/aula02.py
@@ -0,0 +1,10 @@
+print('Pão com queijo')
+nome_usuário = "Brandon Tena" # tipo -> str = string
+idade_usuário = 17 # tipo -> int = integer
+altura_usuário = 1.72 # tipo -> float = ponto flutuante
+maior_idade = False # tipo -> bool = boolean
+
+print('Seja vem vindo', nome_usuário)
+print('Sua idade é', idade_usuário, 'anos,')
+print('Você tem', altura_usuário, 'metros de altura.')
+print('Você é maior de idade?', maior_idade)
